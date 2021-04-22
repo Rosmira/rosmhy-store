28ed7952999b08a7282a51b70a39195d9780ba93
Rosmira <rosmiperez@unac.edu.co>
2021-04-22 14:11:53 -0500

    master page commponent  o nd home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './home/default/default.component';
+import { FooterComponent } from './master-page/master-page/footer/footer.component';
+import { NavbarComponent } from './master-page/master-page/navbar/navbar.component';
+import { SideMenuComponent } from './master-page/master-page/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    FooterComponent,
+    NavbarComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
